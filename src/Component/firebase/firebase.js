12acccf52d04434c026adcae0a5a9d7ef70f0c95
anshulsankhyan <anshulsankhyan@gmail.com>
2023-05-09 15:36:55 +0530

    "code updated"

--- src/Component/firebase/firebase.js
+++ src/Component/firebase/firebase.js
@@ -2,20 +2,19 @@ import firebase from 'firebase/app'
 import 'firebase/storage'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCNvLMimNa3A31sGM6mOl9WqaHgvBLN0MA",
-    authDomain: "outreach-portal-8d2c2.firebaseapp.com",
-    projectId: "outreach-portal-8d2c2",
-    storageBucket: "outreach-portal-8d2c2.appspot.com",
-    messagingSenderId: "971578777854",
-    appId: "1:971578777854:web:d6b885f2a227e5f5db05af"
-  };
-  // Initialize Firebase
+    apiKey: "AIzaSyDT8oeKc89ccuDixway8NzMmslNqgPlVuA",
+    authDomain: "student-sphere-f2213.firebaseapp.com",
+    projectId: "student-sphere-f2213",
+    storageBucket: "student-sphere-f2213.appspot.com",
+    messagingSenderId: "468222313257",
+    appId: "1:468222313257:web:fa0dbf7bbc11dc00ffed38"
+};
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const storage = firebase.storage()
 
 
 export  {
-   storage, firebase as default
- }
-
+    storage, firebase as default
+}
 
